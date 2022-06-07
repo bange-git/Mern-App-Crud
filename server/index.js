e71dfc7e1ccf1d3229742c47313852694dfda9de
bange-git <sebastienbange@gmail.com>
2022-06-07 09:38:36 +0100

    created the first api routes for user authentication

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import morgan from 'morgan';
+import userRouter from './routes/user.js';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send('hello world');
-})
+app.use(morgan("dev"));
+app.use(express.json({ limit: "30mb", extended:true}));
+app.use(express.urlencoded({ limit: "30mb", extended:true}));
+app.use(cors());
+
+
+app.use('/users', userRouter);
+
+
+const MongoDb_url ="mongodb+srv://admin12345:admin12345@cluster0.j7t2o.mongodb.net/tour-db?retryWrites=true&w=majority";
+
 const port = 5000;
-app.listen(port, () => console.log(`
-server is running on ${port}`));
+
+mongoose.connect(MongoDb_url).then(() => {
+  app.listen(port, () => console.log(`server is running on ${port}`) );
+}).catch((error) => console.log(`${error} did not connect`));
+
