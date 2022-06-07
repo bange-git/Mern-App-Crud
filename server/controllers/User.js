27cb660fd6245629ccaaa53fec3f0763990e175e
bange-git <sebastienbange@gmail.com>
2022-06-07 10:02:24 +0100

    added the endpoint for sign in, now moving to create the frontend with react

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -4,6 +4,33 @@ import userModel from '../models/User.js';
 
 const secret = 'test';
 
+export const signin = async (req, res) => {
+const { email, password} =  req.body;
+
+try {
+  const oldUser = await userModel.findOne({email});
+  if(!oldUser){
+    return res.status(404).json({message: "User doesn't exist"});
+  }
+  const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+   if(!isPasswordCorrect){
+     return res.status(400).json({message: "invalid credentials"});
+   }
+   
+   const token = jwt.sign({email:oldUser.email, id:oldUser._id}, secret,{
+     expiresIn:"1h",
+   });
+
+   res.status(200).json({result: oldUser, token});
+
+} catch (error) {
+   res.status(500).json({message: "Something Went Wrong"});
+   console.log(error);
+}
+
+
+}
+
 export const signup = async (req,res) => {
 const { email, password, firstName, lastName } = req.body;
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { signup } from '../controllers/User.js';
+import { signup, signin } from '../controllers/User.js';
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
